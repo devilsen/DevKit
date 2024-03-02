11d69595bd516108cf42ebbd7766d9945876b3ce
dongsen1 <dongsen1@lixiang.com>
2024-03-03 00:05:02 +0800

    init

---
+++ lib/feature/model/category.dart
@@ -0,0 +1,18 @@
+class Category {
+  final String name;
+  final String image;
+
+  Category({required this.name, required this.image});
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      name: json['name'],
+      image: json['image'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'image': image,
+      };
+}
